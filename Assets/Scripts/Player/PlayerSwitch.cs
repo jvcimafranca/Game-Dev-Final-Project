8b676d2e3cab945743e5975241078814e4ed66a5
JV Renz Cimafranca <jccimafranca1@up.edu.ph>
2024-05-11 00:03:34 +0800

    Added changing camera for 2 players

--- Assets/Scripts/Player/PlayerSwitch.cs
+++ Assets/Scripts/Player/PlayerSwitch.cs
@@ -7,11 +7,15 @@ public class PlayerSwitch : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] PlayerController playerController;
     [SerializeField] PlayerController player2Controller;
+    [SerializeField] GameObject camera1;
+    [SerializeField] GameObject camera2;
     private bool player1Active = true;
     void Start()
     {
         // playerController = GetComponent<PlayerController>();
         // player2Controller = GetComponent<PlayerController>();
+        camera1 =  GameObject.FindGameObjectWithTag("State-Driven Camera");
+        camera2 =  GameObject.FindGameObjectWithTag("State-Driven Camera2");
     }
 
     // Update is called once per frame
@@ -30,12 +34,17 @@ public class PlayerSwitch : MonoBehaviour
             playerController.enabled = false;
             player2Controller.enabled = true;
             player1Active = false;
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+
         }
         else
         {
             playerController.enabled = true;
             player2Controller.enabled = false;
             player1Active = true;
+            camera1.SetActive(true);
+            camera2.SetActive(false);
         }
     }
 }
