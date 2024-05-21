2e96c3a08b1c6aeb4a82f2d9935f37752d1ae4d9
JV Renz Cimafranca <jccimafranca1@up.edu.ph>
2024-05-22 01:31:53 +0800

    Added attack animation for Kaden

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject swordPrefab; // Reference to the sword prefab
+    [SerializeField] private GameObject bodyPrefab; // Reference to the body prefab
+    private Animator playerAnimator;
+    private bool isAttacking = false; // To prevent multiple attacks per click
+    [SerializeField] private BoxCollider2D swordCollider;
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        swordPrefab.SetActive(false); // Ensure the sword is initially disabled
+        bodyPrefab.SetActive(false); // Ensure the body is initially disabled
+        swordCollider = swordPrefab.GetComponent<BoxCollider2D>(); // Get the BoxCollider2D component from the sword prefab
+        
+        swordCollider.enabled = false; // Ensure the collider is initially disabled
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&& !isAttacking) // Check if the left mouse button is clicked
+        {
+            Attack();
+        }
+    }
+
+     private void Attack()
+    {
+         isAttacking = true;
+        // Enable the sword and body prefabs
+        swordPrefab.SetActive(true);
+        bodyPrefab.SetActive(true);
+        swordCollider.enabled = true;
+        // Set the isAttacking parameter in the animator to true
+        playerAnimator.SetBool("isAttacking", true);
+
+        // Optionally, you can disable the sword and body prefabs after a delay
+        StartCoroutine(DisableAfterDelay());
+    }
+
+    // Optional: Disable the sword and body after a short delay
+    private IEnumerator DisableAfterDelay()
+    {
+        yield return new WaitForSeconds(0.5f); // Adjust the delay as needed
+        swordPrefab.SetActive(false);
+        bodyPrefab.SetActive(false);
+        swordCollider.enabled = false;
+        // Reset the isAttacking parameter in the animator
+        playerAnimator.SetBool("isAttacking", false);
+        isAttacking = false;
+    }
+}
