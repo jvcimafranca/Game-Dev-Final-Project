8c9d0fc521f5b37da1f459b2befb5db5c3d1cf90
JV Renz Cimafranca <jccimafranca1@up.edu.ph>
2024-05-30 00:53:11 +0800

    added settings

---
+++ Assets/Scripts/Audio/AudioOptionsManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AudioOptionsManager : MonoBehaviour
+{
+    public static float musicVolume { get; private set; }
+    public static float soundEffectsVolume { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI musicSliderText;
+    [SerializeField] private TextMeshProUGUI soundEffectsSliderText;
+
+    public void OnMusicSliderValueChange(float value)
+    {
+        musicVolume = value;
+        
+        musicSliderText.text = ((int)(value * 100)).ToString();
+        AudioManager.Instance.UpdateMixerVolume();
+    }
+
+    public void OnSoundEffectsSliderValueChange(float value)
+    {
+        soundEffectsVolume = value;
+
+        soundEffectsSliderText.text = ((int)(value * 100)).ToString();
+        AudioManager.Instance.UpdateMixerVolume();
+    }
+}
